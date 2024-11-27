10a63101ddaf516d167cdcec684196343a19d40f
IIvell <ivanjosipovic19@gmail.com>
2024-11-27 17:52:42 +0100

    main path

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.asfsdafsfd
+# Create your views here
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('main.urls')), #dodajemo urls
     path('admin/', admin.site.urls),
 ]
