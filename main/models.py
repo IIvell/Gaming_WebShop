73d552d566a70431f18b660bd6a781472125647f
DarioGaming502 <dario.kaisarevic@gmail.com>
2024-11-28 12:29:00 +0100

    Dali radi

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Igrica(models.Model):
+    igrica_naslov = models.CharField(max_length=100)
+    igrica_sadrzaj = models.TextField()
+    igrica_vrijeme_objave = models.DateTimeField('date published')
+
+class Kupac(models.Model):
+    kupac_ime = models.CharField(max_length=25)
+    kupac_prezime = models.CharField(max_length=50)
+    kupac_broj_mobitela = models.CharField(max_length=10)
+    kupac_igrice = models.ManyToManyField(Igrica)
+
 # Create your models here.
