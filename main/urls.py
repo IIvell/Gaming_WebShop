8d272c15d538c450e83bdca972804a8265d49496
DarioGaming502 <dario.kaisarevic@gmail.com>
2024-12-11 21:26:09 +0100

    valjdaradi

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from main.views import CustomLoginView
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -7,4 +9,6 @@ app_name = 'main'  # here for namespacing of urls.
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('', views.homepage, name='homepage'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.views import LoginView
 
 ## Create your views here.
 
@@ -11,4 +12,14 @@ def homepage(request):
     return render(request, 'main/home.html', {})
     # primjetiti korištenje HTML-a
 
-
+class CustomLoginView(LoginView):
+    def form_valid(self, form):
+        # Get the authenticated user
+        user = form.get_user()
+        
+        # Check if the user is a superuser (admin)
+        if user.is_superuser:
+            return redirect('/admin')
+        
+        # Otherwise, proceed with the default behavior
+        return super().form_valid(form)
